02c2f964ac12cea8c3e766db9e08ae1635a0bf71
Meet54trent <120764133+Meet54trent@users.noreply.github.com>
2022-12-16 21:32:31 -0500

    Add files via upload

---
+++ app.R
@@ -0,0 +1,106 @@
+
+# Term Project - Trends in Income and Demographics
+
+
+library(shiny)
+library(tidyverse)
+library(rio)
+library(ggthemes)
+
+person_info=read.csv("personinfo.csv")
+head(person_info)
+
+# Application UI Layout
+ui=shinyUI(fluidPage( style = "background: lightpink",
+  br(),
+  # Application title
+  titlePanel("Trends in Income and Demographics"),
+  p("Explore the differences between those who make less than $50,000 and those who make more than $50,000. You can search for specific demographic data after filtering the data by country."),
+  
+  # Add first fluidRow to select input for country
+  fluidRow(style = "background: lightpink",
+    column(12, 
+           wellPanel(style = "background: lightgreen", selectInput("country","Select Country",choices=c("United-States","Canada","Mexico","Germany","Philippines")))
+    )   
+  ),
+  
+  # Add second fluidRow to control how to plot the continuous variables
+  fluidRow(column(3, 
+                  wellPanel(style = "background: lightblue",
+                    p("Select a continuous variable and graph type (Histogram or Boxplot) to view on the right."),
+                    radioButtons("continuous_variable","Continuous",choices=c("age","hours_per_week")),   
+                    radioButtons("graph_type","Graph",choices=c("Histogram","Boxplot"))    
+                  )
+  ),column(9, plotOutput("p1"))  
+  ),
+  
+  # Add third fluidRow to control how to plot the categorical variables
+  fluidRow(
+    column(3, 
+           wellPanel( style = "background: lightblue",
+             p("Select a categorical variable to view bar chart on the right. Use the check box to view a stacked bar chart to combine the income levels into one graph. "),
+             radioButtons("categorical_variable","Category",choices=c("education","workclass","sex")),    
+             checkboxInput("is_stacked","Stack Bar",value=FALSE)     
+            
+           )
+    ),
+    column(9, plotOutput("p2"))  
+  )
+)
+)
+
+# Define server logic
+server=shinyServer(function(input, output) {
+  person_info=import("personinfo.csv")               
+  names(person_info)=tolower(names(person_info))      
+  
+  df_country <- reactive({
+    person_info %>% filter(native_country == input$country)
+  })
+  
+  # Create logic to plot histogram or boxplot
+  output$p1 <- renderPlot(
+                          {
+    if (input$graph_type == "Histogram") {
+      # Histogram
+      ggplot(df_country(), aes_string(x =input$continuous_variable)) +
+        geom_histogram(color="black",fill="red") +  
+        labs(y="Number of People", title=paste("Trend of ",input$continuous_variable)) +  
+        facet_wrap(~prediction)+ 
+        theme_dark()
+    } else {
+      # Boxplot
+      ggplot(df_country(), aes_string(y = input$continuous_variable)) +
+        geom_boxplot(color="chocolate",fill="darkblue") +  
+        coord_flip() +  
+        labs(x="Number of People", title=paste("Boxplot of",input$continuous_variable)) +  
+        facet_wrap(~prediction)+    
+        theme_dark()
+    }
+    
+  })
+  
+  # Create logic to plot Faceted Bar Chart or Stacked Bar Chart
+
+  output$p2 <- renderPlot( 
+                      {
+    # Bar chart
+    p <- ggplot(df_country(), aes_string(x =input$categorical_variable)) +
+      labs(y="Number of People",title=(paste("Trend of",input$categorical_variable))) +  
+      theme_dark()+
+      theme(axis.text.x=element_text(angle=45),legend.position="bottom")    
+    
+    
+    if (input$is_stacked) {
+      p + geom_bar(aes(fill=prediction))  
+    }
+    else{
+      p + 
+        geom_bar(aes_string(fill=input$categorical_variable)) +  
+        facet_wrap(~prediction)   
+    }
+  })
+
+})
+# Run the application 
+shinyApp(ui = ui, server = server)
